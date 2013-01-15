9ce065fa1ccfe79e9901e57600cfa5144fd036ba
Andrey Beletsky <andrey@vr2.net>
2013-01-15 18:35:01 +0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#encoding=utf-8
+from distutils.core import setup
+import sys
+
+reload(sys).setdefaultencoding("UTF-8")
+
+
+setup(
+    name='retranslate',
+    version='0.1',
+    author='Andrey Beletsky',
+    author_email='andrey@vr2.net',
+    packages=[
+        'retranslate'
+    ],
+    url='http://www.vr2.net',
+    download_url = 'https://github.com/satels/django-qiwi/zipball/master',
+    license = 'MIT license',
+    description = 'A django app to translate non-english strings found in source code to english',
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ),
+    install_requires=['Django>=1.3']
+)
