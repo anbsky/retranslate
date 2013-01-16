d17e7ae82e8413244cd94e350444f1e1340153ca
Andrey Beletsky <andrey@vr2.net>
2013-01-16 15:06:01 +0400

    added functions to process html strings

--- retranslate/utils.py
+++ retranslate/utils.py
@@ -8,50 +8,103 @@ import sys, os
 import re
 import codecs
 
+from bs4 import BeautifulSoup
+
 from models import String
 
 
 U_LITERAL_RE = re.compile(r"u(['\"])(.*?)\1")
-IGNORE_STRINGS = ['{}', '{0}', '%s', '{0} {1}', '%s %s', '\n']
+IGNORE_STRINGS = '{}', '{0}', '%s', '{0} {1}', '%s %s', '\n'
+IGNORE_TAGS = 'style', 'script', 'noscript', '[document]', 'head'
+IGNORE_NODES_RE = re.compile(r'(?:{([%#]).+?\1})|(?:{{.+?}})')
 
 def crawl_folder(path, exclude=None):
     file_count = 0
     for folder, subfolders, files in os.walk(path):
         for file_path in files:
             file_base, file_extension = os.path.splitext(file_path)
-            if file_extension == '.py':
-                full_file_path = os.path.join(folder, file_path)
-                exclude_this = False
-                if exclude:
-                    for name in exclude:
-                        if full_file_path.find(name) > 0:
-                            exclude_this = True
-                            break
-                if not exclude_this:
-                    file_count += 1
-                    extract_from_file(full_file_path)
+            full_file_path = os.path.join(folder, file_path)
+            exclude_this = False
+            if exclude:
+                for name in exclude:
+                    if full_file_path.find(name) > 0:
+                        exclude_this = True
+                        break
+            if not exclude_this and file_extension in FILE_HANDLERS:
+                file_count += 1
+                FILE_HANDLERS[file_extension](full_file_path)
     return file_count
 
-def extract_from_file(file_path):
+def extract_from_py(file_path):
     with codecs.open(file_path, 'r', 'utf-8') as fh:
         ln = 0
         for line in fh:
             ln += 1
             for match in U_LITERAL_RE.finditer(line):
-                handle_token(match, file_path, ln)
+                handle_match(match, file_path, ln)
 
-def handle_token(match, file_path, line):
+def extract_from_html(file_path):
+    with codecs.open(file_path, 'r', 'utf-8') as fh:
+        soup = BeautifulSoup(fh.read())
+
+        for node in filter(visible, soup.findAll(text=True)):
+            node.rstrip()
+            handle_node(node, file_path)
+
+        for node in soup.findAll():
+            for attr, value in node.attrs.items():
+                if isinstance(value, list):
+                    for attr_item in value:
+                        handle_attr(node, attr_item, file_path)
+                else:
+                    handle_attr(node, value, file_path)
+
+FILE_HANDLERS = {
+    '.py': extract_from_py,
+    '.html': extract_from_html
+}
+
+def handle_attr(node, attr_value, file_path):
+    if len(attr_value) and is_non_latin(attr_value):
+        save_string(attr_value, node, file_path)
+
+def is_non_latin(string):
+    for char in string:
+        if ord(char) > 127:
+            return True
+    return False
+
+def visible(element):
+    if element.parent.name in IGNORE_TAGS:
+        return False
+    elif re.match('<!--.*-->', unicode(element)):
+        return False
+    return True
+
+def handle_match(match, file_path, line):
     extracted = match.group(2)
+    # Ignoring empty lines
+    if not extracted or extracted in IGNORE_STRINGS:
+        return
+    location = ':'.join([str(line), str(match.start())])
+    save_string(extracted, match.string, file_path, location)
+
+def handle_node(node, file_path):
+    extracted = node
+    # Ignoring empty lines
+    if not extracted or IGNORE_NODES_RE.search(extracted):
+        return
+    save_string(extracted, node.parent, file_path)
+
+def save_string(string, context, file_path, location=''):
+    string = u'{}'.format(string)
     try:
-        string_obj = String.objects.get(original=extracted)
+        string_obj = String.objects.get(original=string)
     except String.DoesNotExist:
-        # Ignoring empty lines
-        if not extracted or extracted in IGNORE_STRINGS:
-            return
-        location = ':'.join([str(line), str(match.start())])
-        string_obj = String(file=file_path, location=location, original=extracted,
-            context=match.string)
+        string_obj = String(file=file_path, location=location, original=string,
+            context=context)
         string_obj.save()
+
 
 ### FIXME: Below lies unusable ATM code
 
